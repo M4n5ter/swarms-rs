934eadc865bb6f86551c39f1a105979fdadf7f0d
M4n5ter <m4n5terrr@gmail.com>
2025-03-18 15:02:38 +0800

    add swarm_router

--- swarms-rs/src/swarm_router.rs
+++ swarms-rs/src/swarm_router.rs
@@ -1,23 +1,61 @@
-use crate::{agent::Agent, concurrent_workflow::ConcurrentWorkflow, swarm::Swarm};
+use thiserror::Error;
+
+use crate::{
+    agent::Agent,
+    concurrent_workflow::ConcurrentWorkflow,
+    swarm::{Swarm, SwarmError},
+};
+
+#[derive(Debug, Error)]
+pub enum SwarmRouterError {
+    #[error("Swarm Error: {0}")]
+    SwarmError(#[from] SwarmError),
+}
 
 pub struct SwarmRouter {
     name: String,
     description: String,
+    swarm: Option<Box<dyn Swarm>>,
     swarm_type: SwarmType,
     agents: Vec<Box<dyn Agent>>,
 }
 
 impl SwarmRouter {
+    pub fn new(
+        name: impl Into<String>,
+        description: impl Into<String>,
+        swarm_type: SwarmType,
+        agents: Vec<Box<dyn Agent>>,
+    ) -> Self {
+        Self {
+            name: name.into(),
+            description: description.into(),
+            swarm: None,
+            swarm_type,
+            agents,
+        }
+    }
+
+    pub async fn run(
+        &mut self,
+        task: impl Into<String>,
+    ) -> Result<Box<dyn erased_serde::Serialize>, SwarmRouterError> {
+        let task = task.into();
+        self.swarm = Some(self.create_swarm(&task));
+
+        let result = self.swarm.as_ref().unwrap().run(task).await?;
+        Ok(result)
+    }
+
     fn create_swarm(&self, task: &str) -> Box<dyn Swarm> {
         match self.swarm_type {
-            SwarmType::ConcurrentWorkflow => {
-                let workflow = ConcurrentWorkflow::builder()
+            SwarmType::ConcurrentWorkflow => Box::new(
+                ConcurrentWorkflow::builder()
                     .name(&self.name)
                     .description(&self.description)
                     .agents(self.agents.clone())
-                    .build();
-                todo!()
-            }
+                    .build(),
+            ),
             _ => unimplemented!(),
         }
     }
@@ -41,6 +79,7 @@ impl Default for SwarmRouter {
         Self {
             name: "SwarmRouter".to_string(),
             description: "Routes your task to the desired swarm.".to_string(),
+            swarm: None,
             swarm_type: SwarmType::Auto,
             agents: Vec::new(),
         }
